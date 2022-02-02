0590a8303b1c09585e4ad94e64721bc0cc996bdd
Rosemary Ku <rosemary.ku@hotmail.com>
2022-02-02 14:30:02 -0500

    refetch tweets on submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,36 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     user: {
-//       name: "Newton",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@SirIsaac",
-//     },
-//     content: {
-//       text: "If I have seen further it is by standing on the shoulders of giants",
-//     },
-//     created_at: 1461116232227,
-//   },
-//   {
-//     user: {
-//       name: "Descartes",
-//       avatars: "https://i.imgur.com/nlhLi3I.png",
-//       handle: "@rd",
-//     },
-//     content: {
-//       text: "Je pense , donc je suis",
-//     },
-//     created_at: 1461113959088,
-//   },
-// ];
-
 const renderTweets = function (data) {
   $.each(data, function (tweet) {
     let indPost = createTweetElement(data[tweet]);
-    $(".container").append(indPost);
+    $(".tweets-container").prepend(indPost);
   });
 };
 
@@ -55,25 +29,30 @@ const createTweetElement = function (tweet) {
 };
 
 $(document).ready(function () {
-  // renderTweets(data);
-
   $("form").on("submit", function (event) {
     if ($("textarea").val().length === 0) {
       alert("Error your tweet post is empty");
-      event.preventDefault();
-      return;
+      return event.preventDefault();
     }
 
     if ($("textarea").val().length > 140) {
       alert(
         "Your tweet exceeds that maximum character length of 140. Please try tweeting again. "
       );
-      event.preventDefault();
-      return;
+      return event.preventDefault();
     }
 
     event.preventDefault();
-    $.ajax({ url: "/tweets", method: "POST", data: $(this).serialize() });
+    $("container").empty();
+
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: $(this).serialize(),
+    }).then(() => {
+      loadTweets();
+      $("textarea").val("");
+    });
   });
 
   function loadTweets() {
